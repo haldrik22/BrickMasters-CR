a513d10ba5f6ddb6be1859e7b834618a770b936a
Alberto Joxhan Martínez Castro <>
2024-07-24 20:47:49 -0600

    Se realiza prueba de conexión entre el repositorio remoto de GIT y el archivo SQL dentro de Oracle al hacer un cambio.

--- G4_PROYECTO_BRICKDB.sql
+++ G4_PROYECTO_BRICKDB.sql
@@ -1,2 +1,4 @@
 --INICIO: Creación del usuario y esquema para el proyecto. Ambos nombrados G4_PROYECTO_BRICKDB
 --GRANTS CONCEDIDOS: DBA, CONNECT, RESOURCE.
+
+--Probando conexión GIT con Oracle SQL Developer
